c2893be4054420aa6772cd85b350d0ce5bf7806e
Sagnik <santrasagnik10@gmail.com>
2023-05-03 11:37:22 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+
+      <footer class="py-lg-4 py-md-3 py-sm-3 py-3 text-center">
+         <div class="copy-agile-right">
+            <p>artsy</p>
+            <p> 
+             Art Gallery Management System
+            </p>
+            <p>©️ Sagnik</p>
+         </div>
+      </footer>
